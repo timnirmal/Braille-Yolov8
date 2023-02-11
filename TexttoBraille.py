50d0a0cc5f9b3352af53918eacd58467a454bae4
Thimira Nirmal <timnirmal@gmail.com>
2023-02-11 14:31:47 +0530

    Fixed Image locations

--- TexttoBraille.py
+++ TexttoBraille.py
@@ -1,44 +1,14 @@
-# create a gui to select the image and the output file
-
 import tkinter as tk
-from tkinter import filedialog
 from tkinter import *
-from tkinter import ttk
-from tkinter import messagebox
-from tkinter import scrolledtext
-from tkinter import Menu
+from tkinter import filedialog
 import cv2
-
-import os
-import sys
-import time
-import cv2
-import numpy as np
-import json
-import argparse
-import shutil
 from PIL import Image, ImageTk
 from matplotlib import pyplot as plt
 
 from lib import predict
 
-
 openFilePath = r"C:\Users\timni\PycharmProjects\Yolo\datasets\train\images"
 
-
-# create gui
-
-window = tk.Tk()
-window.title("Text to Braille")
-window.geometry("1020x540")
-
-# # create frame
-# frame = tk.Frame(window)
-# frame.grid()
-#
-# # create a label as title in center
-# lbl = tk.Label(window, text="Text to Braille", font=("Arial Bold", 50))
-# lbl.grid()
 
 def pickImage():
     file = filedialog.askopenfilename(initialdir=openFilePath, title='Select File',
@@ -48,20 +18,15 @@ def pickImage():
     # show the image in the gui
     image1 = Image.open(file)
     img = ImageTk.PhotoImage(image1)
-    # panel = tk.Label(window, image=img)
-    # panel.grid(column=0, row=0)
-
-    # img = PhotoImage(file='image 2.png')
-    # label = Label(window, image=img, width=34, height=48, bg='white')
-    label = Label(window, image=img, bg='red')
-    # resize image to fit label size
-    label.image = img
-    label.place(x=84, y=30)
+    label = Label(middle, image=img, bg='red')
+    label.place(x=80, y=80)
 
     # predict the image
     result = predict.predict(file)
 
-    fl = open(r"C:\Users\timni\PycharmProjects\Yolo\runs\detect\predict17\labels\0_2_jpg.rf.76b9c6cba32571b357830b5f3e74db1a.txt", 'r')
+    fl = open(
+        r"C:\Users\timni\PycharmProjects\Yolo\runs\detect\predict17\labels\0_2_jpg.rf.76b9c6cba32571b357830b5f3e74db1a.txt",
+        'r')
     data = fl.readlines()
     fl.close()
 
@@ -83,7 +48,6 @@ def pickImage():
     print("img.shape")
     print(img.shape)
     print()
-
 
     for dt in data:
 
@@ -124,95 +88,72 @@ def pickImage():
 
         cv2.rectangle(img, (l, t), (r, b), (0, 0, 255), 1)
 
-    plt.imshow(img)
-    plt.show()
+    # plt.imshow(img)
+    # plt.show()
 
     # show the image in the gui
     image1 = Image.fromarray(img)
     img = ImageTk.PhotoImage(image1)
-    # panel = tk.Label(window, image=img)
+    # panel = tk.Label(root, image=img)
     # panel.grid(column=0, row=0)
 
     # img = PhotoImage(file='image 2.png')
-    # label = Label(window, image=img, width=34, height=48, bg='white')
-    label = Label(window, image=img, bg='red')
+    # label = Label(root, image=img, width=34, height=48, bg='white')
+    label = Label(middle, image=img, bg='red')
     # resize image to fit label size
     label.image = img
-    label.place(x=84, y=30)
-
+    label.place(x=80, y=80)
 
     # # show the result in the gui
-    # txt = scrolledtext.ScrolledText(window, width=40, height=10)
+    # txt = scrolledtext.ScrolledText(root, width=40, height=10)
     # txt.grid(column=0, row=1)
     # txt.insert(INSERT, result)
     # txt.configure(state='disabled')
 
-
-    # create 3x2 grid of colored circles
-    for row in range(3):
-        for col in range(2):
-            color = "red" if row % 2 == col % 2 else "black"
-            tk.Label(window, text=".", bg=color, width=2, height=1).grid(row=row, column=col)
-
+    # # create 3x2 grid of colored circles
+    # for row in range(3):
+    #     for col in range(2):
+    #         color = "red" if row % 2 == col % 2 else "black"
+    #         tk.Label(middle, text=".", bg=color, width=2, height=1).grid(row=row, column=col)
 
 
+if __name__ == '__main__':
+    # Create Object and setup root
+    root = Tk()
+    root.title("Text to Braille")
+
+    # Create Frames
+    top = Frame(root, width=1440, height=100, bg='white')
+    top.pack(side=TOP)
+    middle = Frame(root, width=1440, height=630, bg='white')
+    middle.pack(side=TOP)
 
 
+    # Create Widgets
+
+    """Top Section"""
+    """Top Section"""
+    """Top Section"""
+
+    # create a label as title in center
+    lbl = tk.Label(top, text="Text to Braille", font=("Arial Bold", 40))
+    lbl.grid()
 
 
+    """Image Section"""
+
+    # label = Label(middle, image=img, width=250, height=303, bg='white')
+
+    # show the image in the gui
+    placeHolderImageOpen = Image.open('test.jpg')
+    placeHolderImage = ImageTk.PhotoImage(placeHolderImageOpen)
+    label = Label(middle, image=placeHolderImage, bg='red', width=416, height=416)
+    label.place(x=80, y=80)
+
+    # Button below the image to pick image and command= pickImage
+    btn = tk.Button(middle, text="Pick Image", font=('arial', 15), width=10, height=1, bg='green', fg='white',command=pickImage)
+    btn.place(x=200, y=530)
 
 
-
-
-
-
-# create a button below the label
-btn = tk.Button(window, text="Click Me", command=pickImage)
-btn.grid(column=0, row=1)
-
-
-
-
-
-
-#
-# # create a label as title in center
-# lbl = tk.Label(window, text="Text to Braille", font=("Arial Bold", 50))
-# lbl.grid()
-#
-#
-# def clicked():
-#     lbl.configure(text="Button was clicked !!")
-#
-# # create a button below the label
-# btn = tk.Button(window, text="Click Me", command=clicked)
-# btn.grid(column=0, row=1)
-#
-# # create a text box
-# txt = tk.Entry(window,width=100)
-# txt.grid(column=0, row=2)
-# txt.focus()
-#
-# # create box to show image test1.jpg
-# image1 = Image.open("test.jpg")
-# img = ImageTk.PhotoImage(image1)
-# panel = tk.Label(window, image = img)
-# panel.grid(column=0, row=3)
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-
-
-
-
-# start the gui
-window.mainloop()
-
-
+    # start the gui
+    root.mainloop()
